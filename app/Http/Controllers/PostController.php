3a6554f592ef2c82fc5329548549b25e324c8ddc
yamayu5 <f2192142@ouhs.ac.jp>
2022-10-14 17:16:26 +0900

    ブログ投稿の作成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 
 
 class PostController extends Controller
@@ -17,5 +18,17 @@ class PostController extends Controller
     public function show(Post $post)
     {
         return view('posts/show')->with(['post' => $post]);
+    }
+    
+    public function create()
+    {
+        return view('posts/create');
+    }
+     
+    public function store(PostRequest $request, Post $post)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
     }
 }
